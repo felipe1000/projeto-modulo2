3a5158370be014ebe4ae6b0e6416f7162503ffb7
Felipe Guedes Maciel <felipeguedes01@hotmail.com>
2015-05-04 17:01:53 -0300

    Primeiros Arquivos bootstrap

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="Projeto- fase 1 - módulo2"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<title>Projeto- fase 2 - módulo2</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/bootstrap-responsive.css">
+	<link rel="stylesheet" href="estilo.css">
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+</head>
+<body>
+
+	<header class="jumbotron subhead">
+		<div class="container">
+			<h1>componentes</h1>
+			<p>Componentes reusaveis para prover navegação, alertas e muito mais</p>
+		</div>
+	</header>
+	<div class="container">
+		<div class="row">
+			<aside class="span3 bs-docs-sidebar">
+				<ul class="nav nav-list bs-docs-sidenav">
+					<li><a href=""><i class="icon-chevron-right"></i> Link 1 </a></li>
+					<li><a href=""><i class="icon-chevron-right"></i> Link 2 </a></li>
+					<li><a href=""><i class="icon-chevron-right"></i> Link 3 </a></li>
+				</ul>
+			</aside>
+		</div>
+	</div>
+
+	<div class="span9">
+		
+	</div>
+		 
+	  <footer class="footer">
+	      <div class="container">
+	      	<p class="pull-right"><a href="#">Voltar ao topo</a></p>
+		        <p>Desenhado e construído com todo amor do mundo por <a href="http://twitter.com/mdo" target="_blank">@mdo</a> e <a href="http://twitter.com/fat" target="_blank">@fat</a>.</p>
+		        <p>Este projeto é uma versão do Twitter Bootstrap da globo.com mantido por <a href="http://blog.alexandremagno.net">Alexandre Magno</a>.</p>
+		        <p>Código licenciado sob <a href="http://www.apache.org/licenses/LICENSE-2.0" target="_blank">Apache License v2.0</a>, documentação sob <a href="http://creativecommons.org/licenses/by/3.0/">CC 3.0</a>.</p>
+		        <p><a href="http://glyphicons.com">Glyphicons grátis</a> licenciado sob <a href="http://creativecommons.org/licenses/by/3.0/">CC 3.0</a>.</p>
+		        <ul class="footer-links">
+		          <li><a href="http://blog.getbootstrap.com">Blog</a></li>
+		          <li class="muted">&middot;</li>
+		          <li><a href="https://github.com/twitter/bootstrap/issues?state=open">Issues</a></li>
+		          <li class="muted">&middot;</li>
+		          <li><a href="https://github.com/twitter/bootstrap/wiki">Roadmap e changelog</a></li>
+		        </ul>
+	      </div>
+	  </footer>
+
+	
+
+</body>
+</html>
