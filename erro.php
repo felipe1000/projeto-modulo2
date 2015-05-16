98fd6982ee5f12b181ec3c80bbc1da142f01c7ea
Felipe Guedes Maciel <felipeguedes01@hotmail.com>
2015-05-16 16:26:54 -0300

    Arquivos .htaccess, erro.php e home.php

---
+++ erro.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="Projeto- fase 1 - módulo2"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<title>Projeto- fase 2 - módulo2</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/bootstrap-responsive.css">
+	<link rel="stylesheet" href="estilo.css">
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+</head>
+<body>
+
+	<?php include_once("header.php"); ?>
+	
+	<div class="container">
+		<div class="row">
+			
+		
+
+		  <div class="span9">
+<?php
+echo "<br><h1>Erro 404</h1>";
+echo "<br><h2>Página não encontrada</h2>";
+echo "<br>Desculpe, mas esta página não está disponivel ou não existe mais!";
+echo "<br><a href='home'>Ir a página Principal (Home)</a>";
+?>
+	  </div>
+
+	    </div>
+    </div>
+    
+    <?php include_once("footer.php"); ?>
+
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="Projeto- fase 1 - módulo2"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<title>Projeto- fase 2 - módulo2</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/bootstrap-responsive.css">
+	<link rel="stylesheet" href="estilo.css">
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+</head>
+<body>
+
+	<?php include_once("header.php"); ?>
+	
+	<div class="container">
+		<div class="row">
+			
+		<?php include_once("menu.php"); ?>
+
+		  <div class="span9">
+
+		  	<?php include_once("home.html"); ?>
+		  	
+		  </div>
+
+	    </div>
+    </div>
+    
+    <?php include_once("footer.php"); ?>
+
+</body>
+</html>
